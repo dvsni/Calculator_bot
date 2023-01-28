4b109020c28aa46c4e932fba724a8fed1618afeb
dvsni <danil.shir@outlook.com>
2023-01-29 01:37:15 +0300

    main

---
+++ main.py
@@ -0,0 +1,7 @@
+from aiogram.utils import executor
+from controller import dp
+
+async def bot_start(_):
+    print('Бот запущен!')
+if __name__ =='__main__':
+    executor.start_polling(dp,skip_updates=True, on_startup=bot_start)
